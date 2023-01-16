46ad7bb82763598332bdbbfc2b4ca34b2465a9d6
M.Inzamam Malik <malikasinger1@gmail.com>
2023-01-17 01:59:44 +0500

    received for new changes such as cancel post

--- admin.py
+++ admin.py
@@ -212,6 +212,7 @@ def save_Data():
     db_store = firestore.client()
     print(request.form)
     dict1 = {}
+    dict1['titleSmall'] = request.form.get("titleSmall")
     dict1['title'] = request.form.get("title")
     dict1['content'] = request.form.get("content")
     dict1['picture'] = request.form.get("picture")
