f7f3bdadf874dbd67e47c1891f9add1446c39891
M Inzamam Malik <malikasinger1@gmail.com>
2023-03-23 11:38:16 +0500

    for hosting

--- admin.py
+++ admin.py
@@ -3,7 +3,7 @@
 
 # In[1]:
 
-
+# importing libraries
 from transformers import AutoModelForSequenceClassification, AutoTokenizer, BertTokenizer, BertForSequenceClassification
 import pandas as pd
 import numpy as np
@@ -17,7 +17,11 @@ from firebase_admin import credentials
 from flask import Flask, request, url_for ,flash , jsonify 
 from firebase_admin import firestore
 import pyrebase
+from datetime import timedelta
+from flask import session, app
 import time
+
+# access database 
 databaseURL = 'https://read-me-a-story3-default-rtdb.firebaseio.com/'
 
 cred_object = credentials.Certificate("./config/firebase.json")
@@ -25,12 +29,14 @@ default_app = firebase_admin.initialize_app(cred_object, {
     'databaseURL': databaseURL,
 })
 
-
+#  How to run flask program reference: https://flask.palletsprojects.com/en/2.2.x/quickstart/#:~:text=To%20run%20the%20application%2C%20use,with%20the%20%2D%2Dapp%20option.&text=As%20a%20shortcut%2C%20if%20the,Line%20Interface%20for%20more%20details
 app = Flask(__name__)
 
+# accessing books collection 
 db = firestore.client()
 todo_ref = db.collection(u'books')
 
+# for admin authentication
 config = {
     'apiKey': "AIzaSyA9f97gGu55wjvB7Nsr75VJHFm71w1b7p0",
     'authDomain': "authenticatepy1.firebaseapp.com",
@@ -75,45 +81,6 @@ class Dataset(torch.utils.data.Dataset):
     def __len__(self):
         return len(self.encodings["input_ids"])
 
-# In[5]:
-
-# def prediction(x):
-#   x_test = ['']
-#   tokenizer = AutoTokenizer.from_pretrained("roberta-base")
-#   X_test_tokenized = tokenizer(x_test, padding=True, truncation=True, max_length=512)
-
-#   # Create torch da
-
-#   test_dataset = Dataset(X_test_tokenized)
-
-#   # Load trained model
-#   model_path = "./classifier_directory"
-#   model = AutoModelForSequenceClassification.from_pretrained(model_path, num_labels=5)
-
-#   # Define test trainer
-#   test_trainer = Trainer(model)
-
-#   # Make prediction
-#   raw_pred, _, _ = test_trainer.predict(test_dataset)
-#   tokenizer = AutoTokenizer.from_pretrained("roberta-base")
-
-
-#   # Preprocess raw predictions
-#   y_pred = np.argmax(raw_pred, axis=1)
-#   if y_pred == 0:
-#     return 'Friendship'
-#   elif y_pred == 1:
-#     return 'Honesty'
-#   elif y_pred == 2:
-#     return 'Patience'
-#   elif y_pred == 3:
-#     return 'Brave'
-#   elif y_pred == 4:
-#     return 'Respect'
-
-# pred=prediction(['afraid,fear,courage,brave,determined '])
-# print(pred)
-
 
 # In[ ]:
 
@@ -131,23 +98,77 @@ def index():
             user = auth.sign_in_with_email_and_password(email, password)
             session['user'] = email
             return redirect(url_for('options'))
+            
         except:
-            flash('Email or Password is incorrect')
+            flash('Email or Password is incorrect','category8')
     return render_template('home.html')
-# log out
 
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    return response
+
+# log out
 
 @app.route('/logout')
 def logout():
-    session.pop('user')
     return redirect('/')
 
-
+# Home page
 @app.route('/options')
 def options():
-    return render_template('options.html')
+     todo_id = request.args.get('id')  
+     if todo_id:
+         todo = todo_ref.document(todo_id).get(field_paths={'moral'})
+         return jsonify(todo.to_dict()), 200
+     else:
+          docs = todo_ref.stream()
+          countP=0
+          countB=0
+          countH=0
+          countF=0
+          countR=0
+          for doc in docs:
+            moral = u'{}'.format(doc.to_dict()['moral'])
+            if moral=="Patience":
+             value = countP + 1
+             countP=value
+             
+            if moral=="Brave":
+             value = countB + 1
+             countB=value
 
-# edit 
+            if moral=="Honesty":
+              value = countH + 1
+              countH=value 
+
+            if moral=="Friendship":
+              value = countF + 1
+              countF=value  
+              
+            if moral=="Respect":
+              value = countR + 1
+              countR=value  
+     total=countP+countB+countF+countH+countR
+
+     #  To display the total number of stories
+     m1="The total is: {}".format(total)   
+     m1=f"The total is: {total} "
+     m1="The total is %d " % (total)
+     m1="The Total Number Of Stories: " + str(total)
+     
+    #  To display the number of stories in each moral in home page 
+    #  How to use flash in flask reference : https://flask.palletsprojects.com/en/2.2.x/patterns/flashing/ 
+     flash(m1 , 'category1')
+     flash(countR, 'category2')
+     flash(countF, 'category3')
+     flash(countH, 'category4')
+     flash(countP, 'category5')
+     flash(countB, 'category6')
+
+     return render_template('options.html')
+
+# Edit 
 
 @app.route('/Edit', methods =["GET","POST"])
 def edit():
@@ -161,7 +182,7 @@ def edit():
           docs = todo_ref.stream()
           for doc in docs:
             title = u'{}'.format(doc.to_dict()['title'])
-            flash(title, 'category2')
+            flash(title, 'category9')
         
         return render_template('edit.html')
 
@@ -171,40 +192,28 @@ def editform():
     return render_template('edit-form.html')
 
 
-    #delete
-@app.route('/Delete', methods =["GET","POST"])
-def delete():
-        #db_store = firestore.client()
-        #users_ref = db_store.collection(u'books')
-        #docs = users_ref.stream()
-    #checklength1 = '<ul>'
-    #for doc in docs:
-     #   checklength1 += '<li>' + doc.title + '</li>'
-    #checklength1 += '</ul>'
-    #register = db.child("books").child("title").get()
-    #registerList = register.val()
-        todo_id = request.args.get('id')  
-        if todo_id:
-         todo = todo_ref.document(todo_id).get(field_paths={'title'})
-         return jsonify(todo.to_dict()), 200
-        else:
-       # all_todos = [doc.get({u'title'}).to_dict() for doc in todo_ref]
-       # for doc in all_todos:
-        #    print(all_todos.get({u'title'}))
-       # return jsonify(all_todos), 200
-          docs = todo_ref.stream()
-          for doc in docs:
-            title = u'{}'.format(doc.to_dict()['title'])
-            flash(title, 'category2')
-        #return jsonify(title)
-        return render_template('delete.html')
-# add
 
+@app.route("/forward/", methods=['POST'])
+def move_forward():
+    #Moving forward code
+    #forward_message = "Moving Forward..."
+    #return render_template('index.html', forward_message=forward_message)
+    title2 = request.form['delete']
+    docs = db.collection(u'books').where("title", "==", title2).get()
+    for doc in docs:
+        key = doc.id
+        db.collection(u'books').document(key).delete()
+    flash('book has been removed successfully', 'category3')
+    return render_template('delete.html')
+
+
+# Add new story
 
 @app.route('/Add', methods=["GET", "POST"])
 def hello():
     return render_template('classifier.html')
 
+# Save data in database after adding new story
 
 @app.route('/Save_data', methods=['GET', 'POST'])
 def save_Data():
@@ -217,12 +226,13 @@ def save_Data():
     dict1['title'] = request.form.get("title")
     dict1['content'] = request.form.get("content")
     dict1['picture'] = request.form.get("picture")
-    dict1['moral'] = request.form.get("moral")  # this shows overload error
+    dict1['moral'] = request.form.get("moral")  
     dict1['createdOn'] = firestore.SERVER_TIMESTAMP
 
     db.collection(u'books').document(curr_time).set(dict1)
     return "save successfully"
 
+# when admin disapproves story it will be saved in canceledStories collection 
 @app.route('/Cancel_data', methods=['GET', 'POST'])
 def cancel_Data():
     print(request.form)
@@ -234,14 +244,14 @@ def cancel_Data():
     dict1['title'] = request.form.get("title")
     dict1['content'] = request.form.get("content")
     dict1['picture'] = request.form.get("picture")
-    dict1['moral'] = request.form.get("moral")  # this shows overload error
+    dict1['moral'] = request.form.get("moral")  
     dict1['createdOn'] = firestore.SERVER_TIMESTAMP
 
     db.collection(u'canceledStories').document(curr_time).set(dict1)
     return "canceled sucessfully"
 
 
-
+# The classifier 
 @app.route('/Add_pred', methods=['GET', 'POST'])
 def prediction():
 
